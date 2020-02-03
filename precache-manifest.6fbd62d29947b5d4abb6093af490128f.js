b98ea012c02e7c75640e0fdabe5de2390c7f409b
rockerhontas <mydaddywasarockstar@gmail.com>
2020-02-04 00:41:32 +0100

    Updates

---
+++ precache-manifest.6fbd62d29947b5d4abb6093af490128f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3d5da1d75f2455ffb0ef0995eb5390",
+    "url": "/tattoolist/index.html"
+  },
+  {
+    "revision": "56e71d7b2c2f5ce57e57",
+    "url": "/tattoolist/static/css/2.9a53c423.chunk.css"
+  },
+  {
+    "revision": "60faedb7dcec8077c5f3",
+    "url": "/tattoolist/static/css/main.c799e1e4.chunk.css"
+  },
+  {
+    "revision": "56e71d7b2c2f5ce57e57",
+    "url": "/tattoolist/static/js/2.f1af9b10.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tattoolist/static/js/2.f1af9b10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60faedb7dcec8077c5f3",
+    "url": "/tattoolist/static/js/main.cbe0d07a.chunk.js"
+  },
+  {
+    "revision": "2c3f82c03841096e0847",
+    "url": "/tattoolist/static/js/runtime-main.3d7e0e28.js"
+  },
+  {
+    "revision": "12a990ec2ea8b2425cbe9beffe6018b0",
+    "url": "/tattoolist/static/media/black_thread.12a990ec.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/tattoolist/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/tattoolist/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/tattoolist/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/tattoolist/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/tattoolist/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/tattoolist/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/tattoolist/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/tattoolist/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/tattoolist/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/tattoolist/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/tattoolist/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/tattoolist/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/tattoolist/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/tattoolist/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/tattoolist/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
